ddbde296bd8a6b6fdaeb825fa7505992d67eb561
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-07-20 14:15:45 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -17,108 +17,284 @@ document.getElementById('demo').innerHTML = "Sunday is a bright day"
 // var a = 89
 // var a = 12
 
-let a = 34
-a = 90
+// let a = 34
+// a = 90
 
-const b = 78
-
-
+// const b = 78
 
 
-console.log(a)
 
-console.log(b)
 
-firstName = "Lynda"
-surName = "Ret"
+// console.log(a)
 
-rat = "oke"
+// console.log(b)
 
-console.log(rat)
+// firstName = "Lynda"
+// surName = "Ret"
+
+// rat = "oke"
+
+// console.log(rat)
+
+// /**
+//  * primitive and reference data types
+//  * 
+//  * number
+//  * string
+//  * boolean
+//  * undefined
+//  * 
+//  * arrays
+//  * objects
+//  * math objects
+//  * 
+//  */
+
+// num1 = 67
+// let sentence = "On the bright side of life"
+// let isMarried = true
+// let cars;
+// let arr1 = ["red", "blue", "green"]
+
+// console.log(typeof arr1)
+// console.log(Array.isArray(arr1))
+
+// let person = {
+//     firstname : "Ada",
+//     age: 30,
+//     hobbies : ["reading", "sleeping"],
+//     isAdmin : false
+// }
+
+// console.log(typeof person)
+
+// //JS OPERATORS
+
+// /**
+//  * assigment = 
+//  * == === equality
+//  * addition, substraction, multp, division comparison, modulus, increment/decrement +, -, /, *, ><>=<=, %, ++, --
+//  */
+
+// mt = "90"
+// xy = 90
+
+
+// console.log(mt === xy)
+
+// let ab = 4
+// let ac = 2
+
+// console.log(ab + ac)
+
+// console.log(ab * ac)
+
+// console.log(ab / ac)
+
+// console.log(ab - ac)
+
+// console.log(ab % ac)
+
+// let num2 = 10
+// let num3 = 16
+// // ++num2
+// // num2++
+// // --num2
+// // num2--
+
+
+
+// console.log(num2 <= num3)
+
+// /**
+//  * LOGICAL OPERATOR
+//  * and && -> the conditions must be true to get true else false
+//  * 
+//  * or || -> when one condition is true everything is true else false
+//  * 
+//  * not ! -> this reverse the true to false and false to true
+//  */
+
+// sum1 = 45
+
+// sum2 = 34
+
+// console.log(sum1 > sum2 && sum1 == sum2)
+
+// console.log (!(sum2 == 45 && sum1 < sum2 || sum1 > sum2))
+
 
 /**
- * primitive and reference data types
+ * conditional statement
  * 
- * number
- * string
- * boolean
- * undefined
+ * if -> the condition must be true to get a result
  * 
- * arrays
- * objects
- * math objects
+ * else if -> the condition must be true to get a result
  * 
+ * else -> this only prints when the if and else if condition is false
  */
 
-num1 = 67
-let sentence = "On the bright side of life"
-let isMarried = true
-let cars;
-let arr1 = ["red", "blue", "green"]
+let a = 23
+let b = 12
 
-console.log(typeof arr1)
-console.log(Array.isArray(arr1))
-
-let person = {
-    firstname : "Ada",
-    age: 30,
-    hobbies : ["reading", "sleeping"],
-    isAdmin : false
+if(a > b){
+    console.log("be like say na him oo")
+}else if(a == b){
+    console.log("dey check am")
+}else if(a > b){
+    console.log("na you sabi")
+}else{
+    console.log("you don jam na!")
 }
 
-console.log(typeof person)
-
-//JS OPERATORS
-
 /**
- * assigment = 
- * == === equality
- * addition, substraction, multp, division comparison, modulus, increment/decrement +, -, /, *, ><>=<=, %, ++, --
+ * staff that i prompted to add their individual salary and i want to add 10,000 to their salaries also print a message according their salary amount
  */
 
-mt = "90"
-xy = 90
 
+// let salary = Number(prompt("enter your salary here"))
 
-console.log(mt === xy)
+// if(salary < 30000){
+//     console.log("low pay")
+// }else if(salary < 40000){
+//     console.log('manageable pay')
+// }else if(salary < 50000){
+//     console.log("you don try pay")
+// }else{
+//     console.log("na you biko!")
+// }
 
-let ab = 4
-let ac = 2
-
-console.log(ab + ac)
-
-console.log(ab * ac)
-
-console.log(ab / ac)
-
-console.log(ab - ac)
-
-console.log(ab % ac)
-
-let num2 = 10
-let num3 = 16
-// ++num2
-// num2++
-// --num2
-// num2--
+// console.log(salary + 10000)
 
 
 
-console.log(num2 <= num3)
+
+// let xy = parseInt("2")
+// let yx = 3
+
+// console.log(xy + yx)
+
 
 /**
- * LOGICAL OPERATOR
- * and && -> the conditions must be true to get true else false
+ * assignment
  * 
- * or || -> when one condition is true everything is true else false
+ * prompt a student to enter his/her scores in 6 courses 
  * 
- * not ! -> this reverse the true to false and false to true
+ * a. calculate the total score
+ * b. calculate the average score
+ * c. calculate the grade
+ * 
+ * <= 39 = F
+ * <= 44 = E
+ * <= 49 = D
+ * <= 59 = C
+ * <= 69 = B
+ * 
+ * else A
+ * 
+ * console.log("Your score is " + totalScore + " and your average is " + avgScore + " finally your grade is " + grade)
  */
 
-sum1 = 45
+// let eng = parseInt(prompt("enter your eng score"))
+// let maths = parseInt(prompt("enter your maths score"))
 
-sum2 = 34
 
-console.log(sum1 > sum2 && sum1 == sum2)
+// total = eng + maths
+// aver = total/2
 
-console.log (!(sum2 == 45 && sum1 < sum2 || sum1 > sum2))
+
+// // console.log(total, aver)
+
+// grade = ""
+
+// if(aver <= 39){
+//     grade = "F"
+// }else if(aver <= 59){
+//     grade = "C"
+// }else{
+//     grade = "A"
+// }
+
+// console.log("Your score is " + total + " and your average is " + aver + " finally your grade is " + grade)
+
+
+/**loop
+ * for 
+ * for/in
+ * for/of
+ * while
+ * do/while
+ */
+
+// debugger;
+
+for(let i = 0; i <= 20; i++){
+    console.log(i)
+}
+
+
+// let str = "Today is saturday"
+
+// for(x = 0; x <str.length; x++){
+//     console.log(str[x])
+// }
+
+
+// let person = {
+//     firstname : "Ada",
+//     age: 30
+// }
+
+// for(let x in person){
+//     console.log(person[x])
+// }
+
+
+// let hobbies =  ["reading", "sleeping"]
+
+// for(i in hobbies){
+//     console.log(hobbies[i])
+// }
+
+// while(i <= 5){
+//     console.log(i)
+//     i++
+// }
+
+// debugger;
+
+// i = 0
+
+// do{
+//     console.log(i)
+//     i++
+// }while(i <= 10)
+
+
+let today = prompt("enter a day to search")
+
+switch(today){
+    case "Tuesday":
+    case "Saturday":
+    case "Apple":
+    
+    console.log("dey search am")
+    break;
+
+    case "Monday":
+    case "Banana":
+    
+    console.log("i don arrive here")
+
+    break;
+
+
+    default:
+        console.log("finally done!")
+}
+
+let age = 18
+
+result = age < 18 ? "you are qualified" : "you are not qualified"
+
+console.log(result)
